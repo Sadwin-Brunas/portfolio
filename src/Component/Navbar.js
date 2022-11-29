78af568300c38ced2e3428a195023ab424dce6e3
Sadwin-Brunas <sadwinbrunas008@gmail.com>
2022-11-29 23:43:45 +0530

    updated portfolio

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -22,7 +22,7 @@ function Navbar() {
                 </button>
         </div>
         <div className="links">
-            <Link to="/">Home</Link>
+            <Link to="/portfolio">Home</Link>
             <Link to="/experience">Experience</Link>
         </div>
     </div>
