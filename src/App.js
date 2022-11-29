c0d6c81b663cc097be791d5bf99345fd21e394e1
Sadwin-Brunas <sadwinbrunas008@gmail.com>
2022-11-29 23:38:00 +0530

    updated portfolio

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
-          <Route path="/portfolio" element={<Home/>}/>
+          <Route path="/" element={<Home/>}/>
           <Route path="/experience" element={<Experience/>}/>
         </Routes>
         <Footer/>
